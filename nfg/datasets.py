49ac34350aa57af14036138582069fbbaf2deed7
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2023-01-11 09:16:35 +0000

    Add epsilon to allow all methods to compare

--- nfg/datasets.py
+++ nfg/datasets.py
@@ -5,6 +5,8 @@ from pycox import datasets
 import pandas as pd
 import numpy as np
 
+EPS = 1e-8
+
 def load_dataset(dataset='SUPPORT', normalize = True, **kwargs):
     if dataset == 'GBSG':
         df = datasets.gbsg.read_df()
@@ -13,15 +15,18 @@ def load_dataset(dataset='SUPPORT', normalize = True, **kwargs):
         df = df.rename(columns = {'x0': 'MKI67', 'x1': 'EGFR', 'x2': 'PGR', 'x3': 'ERBB2', 
                                   'x4': 'Hormone', 'x5': 'Radiotherapy', 'x6': 'Chemotherapy', 'x7': 'ER-positive', 
                                   'x8': 'Age at diagnosis'})
+        df['duration'] += EPS # Avoid problem of the minimum value 0
     elif dataset == 'SYNTHETIC':
         df = datasets.rr_nl_nhp.read_df()
         df = df.drop([c for c in df.columns if 'true' in c], axis = 'columns')
     elif dataset == 'SEER':
         df = pd.read_csv('../data/export.csv')
         df = process_seer(df)
+        df['duration'] += EPS # Avoid problem of the minimum value 0
     elif dataset == 'SYNTHETIC_COMPETING':
         df = pd.read_csv('https://raw.githubusercontent.com/chl8856/DeepHit/master/sample%20data/SYNTHETIC/synthetic_comprisk.csv')
         df = df.drop(columns = ['true_time', 'true_label']).rename(columns = {'label': 'event', 'time': 'duration'})
+        df['duration'] += EPS # Avoid problem of the minimum value 0
     else:
         return load_dsm(dataset, normalize = normalize, **kwargs)
 

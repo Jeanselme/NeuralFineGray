78c5254204ee901b4fac68c30236cc70bd8e6a05
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2023-11-21 11:54:09 -0500

    Avoid notation confusion

--- nfg/losses.py
+++ nfg/losses.py
@@ -5,7 +5,7 @@ import numpy as np
 def total_loss(model, x, t, e):
   # Go through network
   log_sr, log_b, tau = model.forward(x, t)
-  log_hr = model.gradient(log_sr, tau, e)
+  log_hr = model.gradient(log_sr, tau, e).log()
 
   # Compute competing risks
   log_balance_sr = log_b + log_sr
@@ -13,7 +13,7 @@ def total_loss(model, x, t, e):
   # Likelihood error
   error = - torch.logsumexp(log_balance_sr[e == 0], dim = 1).sum() # Sum over the different risks and then across patient
   for k in range(model.risks):
-    error -= (log_balance_sr[e == (k + 1)][:, k] + torch.log(log_hr[e == (k + 1)])).sum() # Sum over patients with this risk
+    error -= (log_balance_sr[e == (k + 1)][:, k] + log_hr[e == (k + 1)]).sum() # Sum over patients with this risk
 
   return error / len(x)
 
